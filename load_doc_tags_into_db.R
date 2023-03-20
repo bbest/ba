dab3258cea5172a561bac7975d128bb3cb0224ae
Ben Best <ben@ecoquants.com>
2023-03-20 23:26:14 +0000

    + tag_excerpt_gpt.py for running in shell

--- load_doc_tags_into_db.R
+++ load_doc_tags_into_db.R
@@ -3,18 +3,22 @@ source("../apps/scripts/db.R")
 shelf(
   dplyr, jsonlite, purrr, tibble, tidyr)
 
-lst <- read_json("doc_tags.json")
-glimpse(lst)
+lst <- read_json("docs_excerpts_tags.json")
+# glimpse(lst)
 # convert list_json1 to a tibble
-d <- as_tibble(lst, validate = F)
+# d <- as_tibble(lst, validate = F)
 
 d <- tibble(
   ba_doc_file = map_chr(lst, 'file_name'),
   excerpt     = map_chr(lst, 'text_excerpt'),
   tag_sql     = map(lst, 'tags')) |>
-  rowid_to_column("rowid")
+  rowid_to_column("id_seq") |>
+  arrange(ba_doc_file, id_seq) |>
+  rowid_to_column("rowid") |>
+  select(-id_seq)
 
-d_ba_excerpts <- d |>
+# dbExecute(con, "DELETE FROM ba_doc_excerpts")
+d_excerpts <- d |>
   select(rowid, ba_doc_file, excerpt)
 dbAppendTable(con, "ba_doc_excerpts", d_excerpts)
 # tbl(con, "ba_doc_excerpts")
---
+++ tag_excerpt_gpt.py
@@ -0,0 +1,125 @@
+# read input argument of an excerpt and return the tags
+# 
+#   python tag_excerpt_gpt.py "{excerpt}" "{mdl_version}"
+#
+# where:
+#   excerpt:     text to tag, should be wrapped in double quotes and have them replaced internally with single quotes
+#   mdl_version: so far either "3" or "4"
+#
+# try in Terminal:
+#   python /share/github/ba/tag_excerpt_gpt.py "Do not approach within 100 feet of whales. If whales approach within 100 feet of your vessel, put engines in neutral and do not re-engage propulsion until whales are observed clear of harm's way from your vessel." "4"
+#
+# install Google Python modules as Shiny on rstudio.marineenergy.app
+# sudo su - shiny
+# pip install --user --upgrade \
+#   python-dotenv \
+#   google-auth \
+#   google-api-python-client \
+#   google-auth-httplib2 \
+#   google-auth-oauthlib \
+#   openai \
+#   tiktoken
+
+import sys
+from pathlib import Path
+import os
+from datetime import datetime
+import time
+import openai
+from dotenv import load_dotenv
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import io
+from googleapiclient.http import MediaIoBaseDownload
+import re
+import tiktoken
+import json
+
+excerpt = sys.argv[1]
+if len(sys.argv) > 2:
+  mdl_version = sys.argv[2]
+else:
+  mdl_version = '3.5'
+  
+# DEBUG
+# excerpt = """
+# Do not approach within 100 feet of whales. If whales approach within 100 feet of 
+# your vessel, put engines in neutral and do not re-engage propulsion until whales 
+# are observed clear of harm's way from your vessel.
+# """
+# excerpt = "Do not approach within 100 feet of whales. If whales approach within 100 feet of your vessel, put engines in neutral and do not re-engage propulsion until whales are observed clear of harm's way from your vessel."
+# GPT-3 (temp=0): Receptor.MarineMammals, Consequence.Injury, Stressor.PhysicalInteraction
+# GPT-4 (temp=0): Receptor.MarineMammals.Cetaceans, Management.Mitigation, Consequence.Collision
+# GPT-4 (temp=0): Consequence.BehavioralChange, Management.Mitigation, Receptor.MarineMammals.Cetaceans, Stressor.BehavioralInteraction.Avoidance
+# GPT-4 (temp=0): Receptor.MarineMammals.Cetaceans, Management.Mitigation, Consequence.Collision
+
+# paths and variables
+script_dir = Path( __file__ ).parent.absolute()
+tags_txt = f'{script_dir}/data/tags.txt'
+# mdl = {
+#   'id'             : 'gpt-3.5-turbo',
+#   'encoding'       : 'gpt-3.5-turbo',
+#   'temperature'    : 0,
+#   'max_tokens'     :  4097,
+#   'max_chunk_nchar': round(4097/3) }
+mdls = {
+  '3.5': {
+    'id'             : 'gpt-3.5-turbo',
+    'encoding'       : 'gpt-3.5-turbo',
+    'temperature'    : 0,
+    'max_tokens'     :  4097,
+    'max_chunk_nchar': round(4097/3) },
+  '4': {
+    'id'             : 'gpt-4',
+    'encoding'       : 'gpt-3.5-turbo',  # ideally 'gpt-4', but not available yet
+    'temperature'    : 0,
+    'max_tokens'     : 8192,
+    'max_chunk_nchar': round(8192/3) } }
+mdl = mdls[mdl_version]
+
+# read tags from tags_txt
+with open(tags_txt, 'r') as f:
+  tags = f.read().strip('\n')
+
+# load OPENAI_API_KEY from .env
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+enc = tiktoken.encoding_for_model(mdl['encoding'])
+
+def get_gpt_chat_response(messages):
+  messages_str    = json.dumps(messages)
+  messages_tokens = len(enc.encode(messages_str))
+  response_tokens = mdl['max_tokens'] - messages_tokens
+  try:
+    response = openai.ChatCompletion.create(
+      model       = mdl['id'],
+      messages    = messages,
+      max_tokens  = response_tokens,
+      n           = 1,
+      temperature = mdl['temperature']) # zero randomness
+    return response.choices[0].message.content.strip()
+  except Exception as e:
+    return f"Error with ChatGPT request: {e}"
+
+initial_message = {
+  'role': 'system',
+  'content': f"""
+    Your task is to judiciously apply a small subset of tags to an excerpt from 
+    a document about environmental impacts around deploying marine renewable 
+    energy devices. Here is the full set of tags:
+    \n
+    {tags}.
+    \n
+    Please provide a comma-seperated sequence tags after I give you the excerpt."""}
+user_message = {
+  'role'   : 'user',
+  'content': f"""
+    Analyze this excerpt for the minimal set of applicable tags:
+    \n
+    {excerpt}
+    """
+}
+messages = [initial_message, user_message]
+response = get_gpt_chat_response(messages)
+print(response)
